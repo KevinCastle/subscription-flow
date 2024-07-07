931cfd0cafb47272bc12c23a2bc5cc6d8139f451
Kevin Castillo <kevinacastles@gmail.com>
2024-07-06 22:09:49 -0400

    feat: header and logo component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  <main class="bg-primary">
-    <RouterView />
+  <main>
+    <section class="container">
+      <header>
+        <LogoCoponent />
+        <ButtonInput
+          label="Solicitar ayuda"
+          text="Ayuda"
+          variant="neutral"
+          type="link"
+          href="https://www.zapping.com/ayuda"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="help-button"
+        />
+      </header>
+      <RouterView />
+    </section>
   </main>
 </template>
 
 <script lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import LogoCoponent from './components/LogoComponent.vue'
+import ButtonInput from './components/ButtonInput.vue'
 
 export default {
+  components: {
+    LogoCoponent,
+    ButtonInput
+  },
   setup() {
     return {
       RouterLink,
