1493651f588ce7e0b0ca74a7262d6a84cb96f482
Kevin Castillo <kevinacastles@gmail.com>
2024-07-07 06:28:39 -0400

    feat: configure i18n

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import es from './locales/es.json'
 
 import App from './App.vue'
 import router from './router'
+
+const i18n = createI18n({
+  locale: 'es',
+  fallbackLocale: 'pr',
+  legacy: false,
+  globalInjection: true,
+  messages: {
+    es
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
